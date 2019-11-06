d064c4189d30037269e946a94a53ed27790301c3
Petr Mifek (EDM) <petr@edmstudio.com>
2019-11-06 11:54:08 +0100

    Keep chosenFile valid when using initial image or chosing a file more than once.

--- src/cropper.vue
+++ src/cropper.vue
@@ -498,14 +498,13 @@ export default {
       this.$refs.fileInput.click()
     },
 
-    remove () {
+    remove (keepChosenFile = false) {
       if (!this.imageSet) return
       this._setPlaceholders()
 
       let hadImage = this.img != null
       this.originalImage = null
       this.img = null
-      this.$refs.fileInput.value = ''
       this.imgData = {
         width: 0,
         height: 0,
@@ -516,7 +515,10 @@ export default {
       this.scaleRatio = null
       this.userMetadata = null
       this.imageSet = false
-      this.chosenFile = null
+      if (!keepChosenFile) {
+        this.$refs.fileInput.value = ''
+        this.chosenFile = null
+      }
       if (this.video) {
         this.video.pause()
         this.video = null
@@ -708,7 +710,7 @@ export default {
 
     _onload (img, orientation = 1, initial) {
       if (this.imageSet) {
-        this.remove()
+        this.remove(true)
       }
       this.originalImage = img
       this.img = img
